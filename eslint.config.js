38bf47f3f00bd1728a20f93c98f63124f8aa7607
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-24 11:20:35 -0300

    Add rule react/prop-types

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
+import { rules } from "eslint-plugin-react/configs/all";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
@@ -9,4 +10,9 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "react/prop-types": [0],
+    },
+  },
 ];
