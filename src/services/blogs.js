2e9cc8a877928459c4d4c8d309d556bc13b6d482
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-24 14:14:41 -0300

    Create post new blog service

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -5,4 +5,10 @@ const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
 };
 
-export default { getAll };
+const create = async (blog, token) => {
+  const headers = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.post(baseUrl, blog, headers);
+  return response.data;
+};
+
+export default { getAll, create };
