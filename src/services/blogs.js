7bf4512a7d31d4ba93473a3360ee75bf42fab0dc
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-26 11:44:59 -0300

    Add updateLikes service

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 const baseUrl = "/api/blogs";
 
+const getHeaders = (token) => {
+  return { headers: { Authorization: `Bearer ${token}` } };
+};
+
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
 };
 
 const create = async (blog, token) => {
-  const headers = { headers: { Authorization: `Bearer ${token}` } };
+  const headers = getHeaders(token);
   const response = await axios.post(baseUrl, blog, headers);
   return response.data;
 };
 
-export default { getAll, create };
+const updateLikes = async (id, likes, token) => {
+  const headers = getHeaders(token);
+  const response = await axios.put(`${baseUrl}/${id}`, likes, headers);
+  return response.data;
+};
+
+export default { getAll, create, updateLikes };
