7fe276a02c88cd6f1f1685124d2078a8318cf5b5
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-07-30 10:25:44 -0300

    Install package and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginVitestGlobals from "eslint-plugin-vitest-globals";
 
 export default [
   { ignores: ["dist/"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    env: {
+      "vitest-globals/env": true,
+    },
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginVitestGlobals.configs.flat.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
